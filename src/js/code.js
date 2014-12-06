2f7c67c18863d1b5f41e5fbf4fc63ff4e1c1d5d2
mclaughta <tom.mclaughlin@gmail.com>
2014-12-05 18:35:48 -0600

    example web-worker code

--- src/js/code.js
+++ src/js/code.js
@@ -1,109 +1,112 @@
-
-/*
- *Licensed under the Apache License, Version 2.0 (the "License");
- *you may not use this file except in compliance with the License.
- *You may obtain a copy of the License at
- *    http://www.apache.org/licenses/LICENSE-2.0
- *Unless required by applicable law or agreed to in writing, software
- *distributed under the License is distributed on an "AS IS" BASIS,
- *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *See the License for the specific language governing permissions and
- *limitations under the License.
- */
-
-/*
- * Adopted from Seth Ladd's excellent Box2D Tutorial
- * http://blog.sethladd.com/2011/09/box2d-javascript-example-walkthrough.html
- */
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
 window.requestAnimFrame = (function(){
-		return  window.requestAnimationFrame       || 
+	return  window.requestAnimationFrame       || 
 		window.webkitRequestAnimationFrame || 
 		window.mozRequestAnimationFrame    || 
 		window.oRequestAnimationFrame      || 
 		window.msRequestAnimationFrame     || 
 		function(/* function */ callback, /* DOMElement */ element){
-			window.setTimeout(callback, 1000 / 60);
-		};
+		window.setTimeout(callback, 1000 / 60);
+	};
 })();
 
-var canvas = document.getElementById('c');
 var SCALE = 30;
 
-world = new Box2D.Dynamics.b2World(
-	new Box2D.Common.Math.b2Vec2(0, 10),    //gravity
-	true                 //allow sleep
-);
-
-
-(function makeGround(){
-	// fixture definition defines attributes of the object, 
-	// e.g. density, friction, restitution (bounciness)
-	var fixDef = new Box2D.Dynamics.b2FixtureDef();
-	fixDef.density = 1.0;
-	fixDef.friction = 0.5;
-	fixDef.restitution = 0.2;
-
-	var bodyDef = new Box2D.Dynamics.b2BodyDef();
-	bodyDef.type = Box2D.Dynamics.b2Body.b2_staticBody;
-
-	// positions the center of the object (not upper left like HTML5 Canvas!)
-	bodyDef.position.x = canvas.width / 2 / SCALE;
-	bodyDef.position.y = canvas.height / SCALE;
-
-	fixDef.shape = new Box2D.Collision.Shapes.b2PolygonShape();
-	fixDef.shape.SetAsBox((600 / SCALE) / 2, (10/SCALE) / 2);
-	world.CreateBody(bodyDef).CreateFixture(fixDef);
-
-})();
-
-(function makeObjects(){
-	var fixDef = new Box2D.Dynamics.b2FixtureDef();
-	fixDef.density = 1.0;
-	fixDef.friction = 0.5;
-	fixDef.restitution = 0.2;
-
-	var bodyDef = new Box2D.Dynamics.b2BodyDef();
-	bodyDef.type = Box2D.Dynamics.b2Body.b2_dynamicBody;
-
-	//make a bunch of random circles or rectangles
-	for(var i = 0; i < 100; ++i) {
-		if(Math.random() > 0.5) {
-			fixDef.shape = new Box2D.Collision.Shapes.b2PolygonShape();
-			fixDef.shape.SetAsBox(
-				Math.random() + 0.1, //half width
-				Math.random() + 0.1 //half height
-			);
-		} else {
-			fixDef.shape = new Box2D.Collision.Shapes.b2CircleShape(
-				Math.random() + 0.1 //radius
-			);
-		}
-		bodyDef.position.x = Math.random() * 25;
-		bodyDef.position.y = Math.random() * 10;
-		world.CreateBody(bodyDef).CreateFixture(fixDef);
-	}
-})();
-
-//setup debug draw
-var debugDraw = new Box2D.Dynamics.b2DebugDraw();
-debugDraw.SetSprite(document.getElementById("c").getContext("2d"));
-debugDraw.SetDrawScale(SCALE);
-debugDraw.SetFillAlpha(0.3);
-debugDraw.SetLineThickness(1.0);
-debugDraw.SetFlags(Box2D.Dynamics.b2DebugDraw.e_shapeBit | Box2D.Dynamics.b2DebugDraw.e_jointBit);
-world.SetDebugDraw(debugDraw);
-
-function update(){
-	world.Step(
-		1 / 60,   //frame-rate
-		10,       //velocity iterations
-		10       //position iterations
-	);
-	world.DrawDebugData();
-	world.ClearForces();
-
-	requestAnimFrame(update);
+function Entity(id, x, y) {
+	this.id = id;
+	this.x = x;
+	this.y = y;
+	this.angle = 0;
 }
 
-requestAnimFrame(update);
+Entity.prototype.update = function(state) {
+	this.x = state.x;
+	this.y = state.y;
+	this.angle = state.a;
+};
+
+Entity.prototype.draw = function(ctx) {
+	ctx.fillStyle = 'black';
+	ctx.beginPath();
+	ctx.arc(this.x * SCALE, this.y * SCALE, 2, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.fill();
+};
+
+function CircleEntity(id, x, y, radius) {
+	Entity.call(this, id, x, y);
+	this.radius = radius;
+}
+CircleEntity.prototype = new Entity();
+CircleEntity.prototype.constructor = CircleEntity;
+
+CircleEntity.prototype.draw = function(ctx) {
+	ctx.fillStyle = 'blue';
+	ctx.beginPath();
+	ctx.arc(this.x * SCALE, this.y * SCALE, this.radius * SCALE, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.fill();
+
+	Entity.prototype.draw.call(this, ctx);
+};
+
+function RectangleEntity(id, x, y, halfWidth, halfHeight) {
+	Entity.call(this, id, x, y);
+	this.halfWidth = halfWidth;
+	this.halfHeight = halfHeight;
+}
+RectangleEntity.prototype = new Entity();
+RectangleEntity.prototype.constructor = RectangleEntity;
+
+ var ctx = document.getElementById("c").getContext("2d");
+
+RectangleEntity.prototype.draw = function(ctx) {
+	ctx.save();
+	ctx.translate(this.x * SCALE, this.y * SCALE);
+	ctx.rotate(this.angle);
+	ctx.translate(-(this.x) * SCALE, -(this.y) * SCALE);
+	ctx.fillStyle = 'red';
+	ctx.fillRect((this.x-this.halfWidth) * SCALE,
+				 (this.y-this.halfHeight) * SCALE,
+				 (this.halfWidth*2) * SCALE,
+				 (this.halfHeight*2) * SCALE);
+				 ctx.restore();
+
+				 Entity.prototype.draw.call(this, ctx);
+};
+
+function randomEntity(id) {
+	var x = Math.random() * 20;
+	var y = Math.random() * 10;
+
+	if (Math.random() > 0.5) {
+		return new CircleEntity(id, x, y, Math.random() + 0.1);
+	} else {
+		return new RectangleEntity(id, x, y, Math.random() + 0.1, Math.random() + 0.1);
+	}
+}
+
+var world = {};
+for (var i = 0; i < 150; i++) {
+	world[i] = randomEntity(i);
+}
+
+
+var worker = new Worker('js/physics.js');
+worker.postMessage(world);
+
+worker.onmessage = function(e) {
+	for (var id in e.data) {
+		world[id].update(e.data[id]);
+	}
+};
+
+(function loop() {
+	ctx.clearRect(0, 0, 640, 480);
+	for (var id in world) {
+		var entity = world[id];
+		entity.draw(ctx);
+	}
+	requestAnimFrame(loop);
+})();
 
